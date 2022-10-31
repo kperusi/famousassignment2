db4569a6f2be0a223a9de7b21ce7737670557842
kperusi <106285754+kperusi@users.noreply.github.com>
2022-10-31 23:19:24 +0100

    1 file modifiednow

--- src/AddComment.js
+++ src/AddComment.js
@@ -1,16 +1,12 @@
+import React from 'react'
 
-import React,{NavLink} from 'react'
-
-export default function ErrorBoundaryPage(props) {
-
-  
+export default function Contact() {
   return (
-
-    <>
-    <h1>{props.message.toUpperCase()}</h1>
-    </>
-   
-    
-    
+    <div>
+      <div className='add-comment'>
+        <h1>Add your comments</h1>
+       <textarea  name="feedback" id="" cols="30" rows="10"></textarea>
+      </div>
+    </div>
   )
 }
